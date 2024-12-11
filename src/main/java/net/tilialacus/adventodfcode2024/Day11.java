218618d6e357aceb4d4aa7a366006d6ea466538c
Roger Lindsjö <roger@tilialacus.net>
2024-12-11 20:54:16 +0100

    Day 11 count stones (total rebuild)

--- src/main/java/net/tilialacus/adventodfcode2024/Day11.java
+++ src/main/java/net/tilialacus/adventodfcode2024/Day11.java
@@ -1,85 +1,61 @@
 package net.tilialacus.adventodfcode2024;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static java.lang.Math.log10;
 import static net.tilialacus.adventodfcode2024.ScanInput.scanInput;
 
 public class Day11 {
     public static void main(String[] args) {
-        Stones stones = new Stones();
+        var stones = buildInput();
+        for (int i = 0; i < 25; i++) {
+            stones = blink(stones);
+        }
+        System.err.println("Day 11 part 1: " + count(stones));
+
+        for (int i = 25; i < 75; i++) {
+            stones = blink(stones);
+        }
+        System.err.println("Day 11 part 2: " + count(stones));
+    }
+
+    private static long count(HashMap<Long, Long> stones) {
+        return stones.values().stream().mapToLong(Long::longValue).sum();
+    }
+
+    private static HashMap<Long, Long> blink(HashMap<Long, Long> stones) {
+        var newStone = new HashMap<Long, Long>();
+        for (Map.Entry<Long, Long> entry : stones.entrySet()) {
+            Long value = entry.getKey();
+            if (value == 0) {
+                newStone.merge(1L, entry.getValue(), Long::sum);
+            } else {
+                var digits = (int) log10(value) + 1;
+                if (digits % 2 == 0) {
+                    int factor = (int) Math.pow(10, digits / 2);
+                    long right = value % factor;
+                    newStone.merge(right, entry.getValue(), Long::sum);
+                    long left = value / factor;
+                    newStone.merge(left, entry.getValue(), Long::sum);
+                } else {
+                    newStone.merge(value * 2024, entry.getValue(), Long::sum);
+                }
+            }
+        }
+        stones = newStone;
+        return stones;
+    }
+
+    private static HashMap<Long, Long> buildInput() {
+        var stones = new HashMap<Long, Long>();
         scanInput("src/main/resources/input-11.txt",
                 scanner -> {
                     while (scanner.hasNextLong()) {
-                        stones.add(new Stone(scanner.nextLong()));
+                        stones.merge(scanner.nextLong(), 1L, Long::sum);
                     }
                 }
         );
-        for (int i = 0; i < 25; i++) {
-            stones.blink();
-        }
-        System.err.println("Day 11 part 1: " + stones.count());
-    }
-
-    static class Stones {
-        Stone first = new Stone(0);
-
-        public void add(Stone stone) {
-            var last = first;
-            while (last.next != null) {
-                last = last.next;
-            }
-            last.next = stone;
-        }
-
-        @Override
-        public String toString() {
-            StringBuilder sb = new StringBuilder();
-            var current = first.next;
-            while (current != null) {
-                sb.append(current.value).append(" ");
-                current = current.next;
-            }
-            return sb.toString();
-        }
-
-        public void blink() {
-            var current = first.next;
-            while (current != null) {
-                if (current.value == 0) {
-                    current.value = 1;
-                } else {
-                    var digits = (int) log10(current.value) + 1;
-                    if (digits % 2 == 0) {
-                        int factor = (int) Math.pow(10, digits / 2);
-                        var s = new Stone(current.value % factor);
-                        s.next = current.next;
-                        current.next = s;
-                        current.value /= factor;
-                        current = current.next;
-                    } else {
-                        current.value *= 2024;
-                    }
-                }
-                current = current.next;
-            }
-        }
-
-        public int count() {
-            int c = 0;
-            var last = first;
-            while (last.next != null) {
-                last = last.next;
-                ++c;
-            }
-            return c;
-        }
-    }
-
-    static class Stone {
-        Stone next;
-        long value;
-
-        public Stone(long value) {
-            this.value = value;
-        }
+        return stones;
     }
 }
