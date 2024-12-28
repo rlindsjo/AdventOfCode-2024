16cd78a00514d6ae231818a07fa5e58566577da8
Roger Lindsjö <roger@tilialacus.net>
2024-12-28 16:00:06 +0100

    Day 13 linear system

--- src/main/java/net/tilialacus/adventodfcode2024/Day13.java
+++ src/main/java/net/tilialacus/adventodfcode2024/Day13.java
@@ -1,5 +1,6 @@
 package net.tilialacus.adventodfcode2024;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -8,7 +9,6 @@ import static java.lang.Integer.parseInt;
 import static net.tilialacus.adventodfcode2024.ScanInput.inputAsLines;
 
 public class Day13 {
-
     public static void main(String[] args) {
         String lines = inputAsLines("src/main/resources/input-13.txt").stream().collect(Collectors.joining("\n"));
         Pattern pattern = Pattern.compile("Button A: X\\+(\\d+), Y\\+(\\d+)\\nButton B: X\\+(\\d+), Y\\+(\\d+)\\nPrize: X=(\\d+), Y=(\\d+)", Pattern.MULTILINE);
@@ -16,34 +16,30 @@ public class Day13 {
         var problems = new ArrayList<Problem>();
 
         while (matcher.find()) {
+            BigDecimal offset = BigDecimal.valueOf(10000000000000L);
             problems.add(new Problem(
-                    new Button(parseInt(matcher.group(1)), parseInt(matcher.group(2))),
-                    new Button(parseInt(matcher.group(3)), parseInt(matcher.group(4))),
-                    parseInt(matcher.group(5)), parseInt(matcher.group(6))));
+                    new Button(new BigDecimal(matcher.group(1)), new BigDecimal(matcher.group(2))),
+                    new Button(new BigDecimal(matcher.group(3)), new BigDecimal(matcher.group(4))),
+                    new BigDecimal(matcher.group(5)).add(offset), new BigDecimal(matcher.group(6)).add(offset)));
         }
 
-        var tokens = 0;
+        var tokens = 0L;
         for (Problem problem : problems) {
-            var q = Integer.MAX_VALUE;
-            for (int a = 0; a <= 100; a++) {
-                if (problem.a().x() > problem.x() || problem.a().y() > problem.y()) {
-                    break;
-                }
-                var b = (problem.x() - a * problem.a().x()) / problem.b().x();
+            try {
+                BigDecimal divisor = problem.a().x().multiply(problem.b().y()).subtract(problem.a().y().multiply(problem.b().x()));
+                var a = problem.b().y().multiply(problem.x()).subtract(problem.y().multiply(problem.b().x()))
+                        .divide(divisor);
+                var b = problem.a().x().multiply(problem.y()).subtract(problem.x().multiply(problem.a().y()))
+                        .divide(divisor);
+                tokens += a.longValue() * 3 + b.longValue();
+            } catch (ArithmeticException e) {
+                // Only integer result
+            }
 
-                if (problem.a().x() * a + problem.b().x() * b == problem.x() && problem.a().y() * a + problem.b().y() * b == problem.y()) {
-                    if (a * 3 + b < q) {
-                        q = a * 3 + b;
-                    }
-                }
-            }
-            if (q < Integer.MAX_VALUE) {
-                tokens += q;
-            }
         }
         System.err.println(tokens);
     }
 
-    record Button(int x, int y) {}
-    record Problem(Button a, Button b, int x, int y) {}
+    record Button(BigDecimal x, BigDecimal y) {}
+    record Problem(Button a, Button b, BigDecimal x, BigDecimal y) {}
 }
